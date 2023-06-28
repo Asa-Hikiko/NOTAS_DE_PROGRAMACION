4cfffa9cd644dccc67f367fa3f8134d03a05fe95
AlbertSteel <camposyapu@gmail.com>
2023-06-27 23:59:51 -0500

    nuevo comentario

--- Nueva carpeta/COMENTARIOS MULTI LINEA.py
+++ Nueva carpeta/COMENTARIOS MULTI LINEA.py
@@ -1,7 +1,21 @@
 #esto es un comentario
-x= 105//4 # es division entera por lo que deberia salir  26 pero si es una division normal seria 26.25
-print( x)
+x = (105 // 4)  # es division entera por lo que deberia salir  26 pero si es una division normal seria 26.25
+print(x)
 "esto es un comentario o un valor nulo"
 """esto es
 comentario multi 
 lineas"""
+
+
+#! otra cadena   con """  pero por debajo de una funcion
+
+
+def shout(word):
+    """
+    Print a word with an
+    exclamation mark following it.
+    """
+    print(word + "!")
+
+
+shout("spam")
