57856fe7571a00b87664175d6f66a70bfab53f2e
AlbertSteel <camposyapu@gmail.com>
2024-03-15 11:12:49 -0500

    Create PROBANDO STRIP( ).py

---
+++ Nueva carpeta/PROBANDO STRIP( ).py
@@ -0,0 +1,21 @@
+cadena = "\tHola Ernesto\n"
+print(cadena)
+
+cadena = cadena.strip()
+print(cadena)
+
+print("///////////////////////////////////////////////////")
+
+cadena = "\tHola Ernesto\n"
+print(cadena)
+
+cadena = cadena.strip("s tHo")
+print(cadena)
+
+print("///////////////////////////////////////////////////")
+
+cadena = "\tHola Ernesto\n"
+print(cadena)
+
+cadena = cadena.strip("s tHo\t\n")
+print(cadena)
